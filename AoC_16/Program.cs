63dd0b95dc6898291a646ca84a378256e6947284
aamartin <aamartin@gmail.com>
2020-12-28 18:22:41 -0800

    Adding solutions for days 16 and 17

---
+++ AoC_16/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace AoC_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            int errors = 0;
+            var allValid = new HashSet<int>();
+            var feilds = new List<HashSet<int>>();
+            var validTickets = new List<string>();
+            List<int> departureFeilds = new List<int>();
+            var highestFieldValue = -1;
+            string myTicket = string.Empty;
+            int section = 0;
+            int index = 0;
+            foreach(var line in lines)
+            {
+                if(String.IsNullOrEmpty(line))
+                {
+                    section++;
+                    continue;
+                }
+                if (section == 0) // Init block
+                {
+                    if(line.Contains("departure")) { departureFeilds.Add(index); }
+                    var parts = line.Split(' ');
+                    var validFields = new HashSet<int>();
+                    foreach (var part in parts)
+                    {
+                        if (part.Contains("-"))
+                        {
+                            var startend = part.Split('-');
+                            for (int i = int.Parse(startend[0]); i <= int.Parse(startend[1]); i++)
+                            {
+                                allValid.Add(i);
+                                validFields.Add(i);
+                                if (i > highestFieldValue) highestFieldValue = i;
+                            }
+                        }
+                    }
+                    
+                    feilds.Add(validFields);
+                    index++;
+                }
+                else if (section == 1)
+                {
+                    if (line.Contains(":")) continue;
+                    myTicket = line;
+                }
+                else if (section == 2)
+                {
+                    if (line.Contains(":")) continue;
+                    var values = line.Split(',');
+                    bool validTicket = true;
+                    foreach (var value in values)
+                    {
+                        var intValue = int.Parse(value);
+                        if (!allValid.Contains(intValue))
+                        {
+                            errors = errors + intValue;
+                            validTicket = false;
+                        }
+                    }
+                    if (validTicket)
+                    {
+                        validTickets.Add(line);
+                    }
+                }
+            }
+            Console.WriteLine($"Part 1: {errors}");
+
+            var part2 = Part2(myTicket, feilds, validTickets, highestFieldValue);
+            var myTicketParts = myTicket.Split(',');
+            long part2Answer = 1;
+            foreach(var departureField in departureFeilds)
+            {
+                part2Answer *= int.Parse(myTicketParts[part2[departureField]]);
+            }
+
+            Console.WriteLine($"Part 2: {part2Answer}");
+        }
+
+        private static int[] Part2(string myTicket, List<HashSet<int>> feilds, List<string> validTickets, int highestFieldValue)
+        {
+            // Build a bitmask for each feild that could contain a valid feild
+            int[] possiblefieldsByValue = new int[highestFieldValue+1];
+            for(int i = 0; i < feilds.Count; i++)
+            {
+                int flag = 1 << i;
+                foreach(var field in feilds[i])
+                {
+                    possiblefieldsByValue[field] |= flag;
+                }
+            }
+
+            // initilize a bitmap table that stores the feild per ticket mapping.
+            var fieldCandidates = new int[feilds.Count];
+            for(int i = 0; i < feilds.Count; i++)
+                fieldCandidates[i] = -1;
+
+            foreach(var ticket in validTickets)
+            {
+                var ticketParts = ticket.Split(',');
+                for(int i = 0; i < feilds.Count; i++)
+                {
+                    fieldCandidates[i] &= possiblefieldsByValue[int.Parse(ticketParts[i])];
+                }
+            }
+
+            return GetFieldIndexes(fieldCandidates);
+        }
+
+        public static int CountTrailingZero(int x)
+        {
+            int count = 0;
+
+            while ((x & 1) == 0)
+            {
+                x = x >> 1;
+                count++;
+            }
+            return count;
+        }
+
+        private static int[] GetFieldIndexes(int[] candidates)
+        {
+            int[] fieldIndexes = new int[candidates.Length];
+
+            // This loop will identify a single field index each iteration
+            // so this loop runs candidates.Length times 
+            for (int fieldsLeft = candidates.Length; fieldsLeft > 0; fieldsLeft--)
+            {
+                int fieldToRemove = 0;
+                for (int j = 0; j < candidates.Length; j++)
+                {
+                    int fields = candidates[j];
+
+                    // if we have already identified the field index, then fields will be 0 so we skip it
+                    if (fields == 0)
+                        continue;
+
+                    // x & (x - 1) == 0 is a way to test that x only has 1 bit set
+                    // when there is only 1 bit set, it means we have identified the field index
+                    if ((fields & (fields - 1)) == 0)
+                    {
+                        // TrailingZeroCount is a fast way to identify which bit is set to 1
+                        int field = CountTrailingZero(fields);
+                        fieldIndexes[field] = j;
+                        fieldToRemove = fields;
+                        break;
+                    }
+                }
+
+                // remove the field from each of the candidates now that we have identified it's field index
+                for (int j = 0; j < candidates.Length; j++)
+                {
+                    candidates[j] &= ~fieldToRemove;
+                }
+            }
+
+            return fieldIndexes;
+        }
+    }
+}
---
+++ AoC_17/Program.cs
@@ -0,0 +1,181 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+
+namespace AoC_17
+{
+    class Program
+    {
+        static int maxdim = 30;
+        
+        static void Main(string[] args)
+        {
+
+            var lines = File.ReadAllLines("input.txt");
+            bool[,,] pocketSpace = new bool[maxdim, maxdim, maxdim];
+            bool[,,,] pocketSpace4d = new bool[maxdim, maxdim, maxdim, maxdim];
+            var midSlice = (int)Math.Floor((double)maxdim/2);
+            int y = 0;
+            foreach(var line in lines)
+            {
+                var parts = line.ToCharArray();
+                for (int x = 0; x < parts.Length; x++)
+                {
+                    if (parts[x] == '#') pocketSpace[midSlice + x-parts.Length, midSlice + y - parts.Length, midSlice] = true;
+                    if (parts[x] == '#') pocketSpace4d[midSlice + x - parts.Length, midSlice + y - parts.Length, midSlice, midSlice] = true;
+                }
+                y++;
+            }
+            
+            for(int i = 0; i< 6; i++)
+            {
+                pocketSpace = Simulate(pocketSpace);
+                pocketSpace4d = Simulate4D(pocketSpace4d);
+            }
+            
+            int total = 0;
+            for(int dx =0; dx < maxdim; dx++)
+            {
+                for(int dy = 0; dy < maxdim; dy++)
+                {
+                    for(int dz =0; dz < maxdim; dz++)
+                    {
+                        if (pocketSpace[dx, dy, dz])
+                        {
+                            total++;
+                        }
+                    }
+                }
+            }
+
+            int totalPart2 = 0;
+            for (int dx = 0; dx < maxdim; dx++)
+            {
+                for (int dy = 0; dy < maxdim; dy++)
+                {
+                    for (int dz = 0; dz < maxdim; dz++)
+                    {
+                        for (int dw = 0; dw < maxdim; dw++)
+                        {
+                            if (pocketSpace4d[dx, dy, dz, dw])
+                            {
+                                totalPart2++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Part 1: {total}");
+            Console.WriteLine($"Part 2: {totalPart2}");
+
+        }
+
+        private static bool[,,] Simulate(bool[,,] pocketSpace)
+        {
+            var newSpace = new bool[maxdim, maxdim, maxdim];
+            for(int x = 1; x< maxdim-2; x++)
+            {
+                for(int y = 1; y < maxdim-2; y++)
+                {
+                    for(int z = 1; z < maxdim-2; z++)
+                    {
+                        var activeNeighboors = CountNeighboors(pocketSpace, x, y, z);
+                        if(pocketSpace[x,y,z] && activeNeighboors >= 2 && activeNeighboors <= 3 )
+                        {
+                            newSpace[x, y, z] = true;
+                        }
+                        else if(!pocketSpace[x,y,z] && activeNeighboors == 3)
+                        {
+                            newSpace[x, y, z] = true;
+                        }
+                        else { newSpace[x, y, z] = false; }
+                    }
+                }
+            }
+            return newSpace;
+        }
+
+        private static bool[,,,] Simulate4D(bool[,,,] pocketSpace)
+        {
+            var newSpace = new bool[maxdim, maxdim, maxdim ,maxdim];
+            for (int x = 1; x < maxdim - 2; x++)
+            {
+                for (int y = 1; y < maxdim - 2; y++)
+                {
+                    for (int z = 1; z < maxdim - 2; z++)
+                    {
+                        for (int w = 1; w < maxdim - 2; w++)
+                        {
+                            var activeNeighboors = Count4dNeighboors(pocketSpace, x, y, z, w);
+                            if (pocketSpace[x, y, z, w] && activeNeighboors >= 2 && activeNeighboors <= 3)
+                            {
+                                newSpace[x, y, z, w] = true;
+                            }
+                            else if (!pocketSpace[x, y, z, w] && activeNeighboors == 3)
+                            {
+                                newSpace[x, y, z, w] = true;
+                            }
+                            else { newSpace[x, y, z, w] = false; }
+                        }
+                    }
+                }
+            }
+            return newSpace;
+        }
+
+        private static int CountNeighboors(bool[,,] pocketSpace, int x, int y, int z)
+        {
+
+            var activeNeigboors = 0;
+            foreach (var dx in Enumerable.Range(-1, 3))
+                foreach (var dy in Enumerable.Range(-1, 3))
+                    foreach (var dz in Enumerable.Range(-1, 3))
+                    {
+                        if (dx == 0 && dy == 0&& dz == 0)
+                            continue;
+                        if (pocketSpace[dx + x, dy + y, dz + z]) activeNeigboors++;
+                    }
+
+            return activeNeigboors;
+        }
+
+        private static int Count4dNeighboors(bool[,,,] pocketSpace, int x, int y, int z, int w)
+        {
+            var activeNeigboors = 0;
+            foreach (var dx in Enumerable.Range(-1, 3))
+                foreach (var dy in Enumerable.Range(-1, 3))
+                    foreach (var dz in Enumerable.Range(-1, 3))
+                        foreach (var dw in Enumerable.Range(-1, 3))
+                        {
+                        if (dx == 0 && dy == 0 && dz == 0 && dw == 0)
+                            continue;
+                        if (pocketSpace[dx + x, dy + y, dz + z, dw + w]) activeNeigboors++;
+                    }
+
+            return activeNeigboors;
+        }
+
+        private static void PrintSlice(bool[,,] pocketSpace, int z)
+        {
+            Console.WriteLine(" 012345678901234567890123456789" );
+            for (int y = 0; y < maxdim; y++)
+            {
+                Console.Write(y);
+                for(int x = 0; x < maxdim; x++)
+                {
+                    if (pocketSpace[x, y, z]) Console.Write("#");
+                    else { Console.Write("."); }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
